53b8f1db0d525eb6d621dc7ece3c4579e53d5b58
KDH20160140 <bzidol@naver.com>
2022-10-03 20:15:30 +0900

    연습문제 > 피보나치 수

--- 프로그래머스/LV2/221003_5.js
+++ 프로그래머스/LV2/221003_5.js
@@ -6,7 +6,7 @@ function solution(n) {
     fibonacci[1] = 1;
 
     // 1234567로 나눈 n 번째 피보나치 수를 출력함.
-    // 정수형을 벗어나기 때문에 매번 1234567 보다 작은수로 값을 변경한다.
+    // 정수형을 벗어나기 때문에 매번 1234567 보다 작은수로 값을 변경.
     for (let idx = 2; idx <= n; idx++) {
         fibonacci[idx] = (fibonacci[idx - 1] + fibonacci[idx - 2]) % 1234567;
     }
